2ef82d580fbe793be252f28aed3d2c9ad604c389
Thiago Peñaloza <thiagoalmeidapenaloza@gmail.com>
2023-04-15 23:03:25 -0300

    99% finished - only the styling of the dynamic div is missing

--- script.js
+++ script.js
@@ -2,6 +2,11 @@ const slides = document.querySelectorAll('[data-js="carousel__item"]');
 const nextButton = document.querySelector('[data-js="carousel__button--next"]');
 const prevButton = document.querySelector('[data-js="carousel__button--prev"]');
 const smButton = document.querySelector('[data-js="carousel__SM"]');
+const motoInfo = document.querySelector('[data-js="moto-info"]');
+const main = document.querySelector("main");
+const nav = document.querySelector("nav");
+
+const saibaMaisButton = document.querySelector("#buttonSM");
 
 const lastSlideIndex = slides.length - 1;
 let currentSlideIndex = 0;
@@ -27,4 +32,52 @@ prevButton.addEventListener("click", () => {
       : --currentSlideIndex;
 
   manipulateSlidesClasses(correctSlideIndex);
+});
+
+const motoData = [
+  {
+    name: "Scooter Elétrica Voltz EV1",
+    info: "A scooter elétrica Voltz EV1 oferece uma pilotagem suave e confortável com transmissão automática. Com motor elétrico eficiente e freios ABS, é uma opção sustentável e segura para deslocamentos urbanos!",
+    img: "moto1.svg",
+    id: "moto1",
+  },
+  {
+    name: "Honda CB 500X",
+    info: "O câmbio de 6 marchas da CB 500X oferece maior confiança e performance na pilotagem, tornando as subidas de marcha mais leves e as reduções mais suaves!",
+    img: "moto2.svg",
+    id: "moto2",
+  },
+];
+smButton.addEventListener("click", () => {
+  const currentMotoData = motoData[currentSlideIndex];
+
+  motoInfo.innerHTML = `
+     <div class="nav-moto">
+     <h3>${currentMotoData.name}</h3>
+     <button class="close-button" data-js="close-button"">
+     X
+     </button>
+     </div>
+     <p>${currentMotoData.info}</p>
+      <img id="${currentMotoData.id}" src="${currentMotoData.img}" />
+      <button id="assignButton">Quero Assinar!</button>
+    `;
+
+  motoInfo.classList.add("moto-info--visible");
+
+  const closeButton = document.querySelector('[data-js="close-button"]');
+
+  closeButton.addEventListener("click", () => {
+    main.classList.remove("modal-open");
+    nav.classList.remove("modal-open");
+    motoInfo.remove();
+  });
+});
+
+const body = document.querySelector("body");
+
+saibaMaisButton.addEventListener("click", () => {
+  main.classList.add("modal-open");
+  nav.classList.add("modal-open");
+  body.appendChild(motoInfo);
 });
