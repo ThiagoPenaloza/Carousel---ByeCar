8c76b1d4d655bdc90e75d9d8f612eb5de26fd8e1
Thiago Peñaloza <thiagoalmeidapenaloza@gmail.com>
2023-04-18 19:30:39 -0300

    organização de código script completo

--- script.js
+++ script.js
@@ -1,40 +1,3 @@
-const slides = document.querySelectorAll('[data-js="carousel__item"]');
-const nextButton = document.querySelector('[data-js="carousel__button--next"]');
-const prevButton = document.querySelector('[data-js="carousel__button--prev"]');
-const smButton = document.querySelector('[data-js="carousel__SM"]');
-const motoInfo = document.querySelector('[data-js="moto-info"]');
-const main = document.querySelector("main");
-const nav = document.querySelector("nav");
-
-const saibaMaisButton = document.querySelector("#buttonSM");
-
-const lastSlideIndex = slides.length - 1;
-let currentSlideIndex = 0;
-
-const manipulateSlidesClasses = (correctSlideIndex) => {
-  slides.forEach((slide) => slide.classList.remove("carousel__item--visible"));
-  slides[currentSlideIndex].classList.add("carousel__item--visible");
-};
-
-nextButton.addEventListener("click", () => {
-  const correctSlideIndex =
-    currentSlideIndex === lastSlideIndex
-      ? (currentSlideIndex = 0)
-      : ++currentSlideIndex;
-
-  manipulateSlidesClasses(correctSlideIndex);
-});
-
-prevButton.addEventListener("click", () => {
-  const correctSlideIndex =
-    currentSlideIndex === 0
-      ? (currentSlideIndex = lastSlideIndex)
-
-      : --currentSlideIndex;
-
-  manipulateSlidesClasses(correctSlideIndex);
-});
-
 const motoData = [
   {
     name: "Scooter Elétrica Voltz EV1",
@@ -49,25 +12,81 @@ const motoData = [
     id: "moto2",
   },
 ];
-smButton.addEventListener("click", () => {
-  const currentMotoData = motoData[currentSlideIndex];
+const nextButton = document.querySelector('[data-js="carousel__button--next"]');
+const prevButton = document.querySelector('[data-js="carousel__button--prev"]');
+const smButton = document.querySelector('[data-js="carousel__SM"]');
+const motoInfo = document.querySelector('[data-js="moto-info"]');
+const main = document.querySelector("main");
+const nav = document.querySelector("nav");
+const slides = document.querySelectorAll('[data-js="carousel__item"]');
+const body = document.querySelector("body");
 
-  motoInfo.innerHTML = `
-     <div class="nav-moto">
-     <h3>${currentMotoData.name}</h3>
-     <button class="close-button" data-js="close-button"">
-     X
-     </button>
-     </div>
-     <p>${currentMotoData.info}</p>
-      <img id="${currentMotoData.id}" src="${currentMotoData.img}" />
-      <button id="assignButton">Quero Assinar!</button>
-    `;
+const saibaMaisButton = document.querySelector("#buttonSM");
 
-  motoInfo.classList.add("moto-info--visible");
-  motoInfo.classList.remove("fade-out");
+const lastSlideIndex = slides.length - 1;
+let currentSlideIndex = 0;
+
+function showCarrossel() {
+  const manipulateSlidesClasses = (correctSlideIndex) => {
+    slides.forEach((slide) => slide.classList.remove("carousel__item--visible"));
+    slides[currentSlideIndex].classList.add("carousel__item--visible");
+  };
+  manipulateSlidesClasses();
+}
+showCarrossel();
+
+function passarCarrossel() {
+  nextButton.addEventListener("click", () => {
+    const correctSlideIndex =
+      currentSlideIndex === lastSlideIndex
+        ? (currentSlideIndex = 0)
+        : ++currentSlideIndex;
+
+    showCarrossel();
+  });
+}
+passarCarrossel();
+
+function voltarCarrossel() {
+  prevButton.addEventListener("click", () => {
+    const correctSlideIndex =
+      currentSlideIndex === 0
+        ? (currentSlideIndex = lastSlideIndex)
+
+        : --currentSlideIndex;
+
+    showCarrossel();
+  });
+}
+voltarCarrossel();
+
+
+function openModal() {
+  smButton.addEventListener("click", () => {
+    const currentMotoData = motoData[currentSlideIndex];
+
+    motoInfo.innerHTML = `
+       <div class="nav-moto">
+       <h3>${currentMotoData.name}</h3>
+       <button class="close-button" data-js="close-button"">
+       X
+       </button>
+       </div>
+       <p>${currentMotoData.info}</p>
+        <img id="${currentMotoData.id}" src="${currentMotoData.img}" />
+        <button id="assignButton">Quero Assinar!</button>
+      `;
+
+    motoInfo.classList.add("moto-info--visible");
+    motoInfo.classList.remove("fade-out");
+    closeModal();
+  });
+}
+openModal();
+
+function closeModal() {
+  openModal();
   const closeButton = document.querySelector('[data-js="close-button"]');
-
   closeButton.addEventListener("click", () => {
     motoInfo.classList.add("fade-out");
     main.classList.add("blur-out");
@@ -79,14 +98,15 @@ smButton.addEventListener("click", () => {
       motoInfo.remove();
     }, 500)
   });
-});
+}
 
-const body = document.querySelector("body");
-
-saibaMaisButton.addEventListener("click", () => {
-  main.classList.remove("blur-out");
-  nav.classList.remove("blur-out");
-  main.classList.add("modal-open");
-  nav.classList.add("modal-open");
-  body.appendChild(motoInfo);
-});
+function blurOnOff() {
+  saibaMaisButton.addEventListener("click", () => {
+    main.classList.remove("blur-out");
+    nav.classList.remove("blur-out");
+    main.classList.add("modal-open");
+    nav.classList.add("modal-open");
+    body.appendChild(motoInfo);
+  });
+}
+blurOnOff();
